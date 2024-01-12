b15d5fc0c675bbf74734677f8f8f9f4aed795821
jcdominguez <jcd.pro@gmail.com>
2024-01-12 13:43:06 +0100

    first commit

---
+++ src/main/java/com/demo/MessageBean.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class MessageBean implements Serializable {
+
+    private int compteur = 0;
+
+    private String message = "Mon 1er Bean CDI";
+
+    public String getMessage() {
+        compteur++;
+        return message + " "+compteur;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCompteur() {
+        return compteur;
+    }
+
+    public void setCompteur(int compteur) {
+        this.compteur = compteur;
+    }
+}
